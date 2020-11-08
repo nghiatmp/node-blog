f8814e27bc8bdd5732b26a152ae2c1bd18688f97
NghiaLM <nghialm@hblab.vn>
2020-11-08 10:26:08 +0700

    add model

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,11 @@ const siteRouter = require('./site');
 function routes(app) {
     app.use('/news', newsRouter);
 
-    app.use('/',siteRouter);
+    app.use('/', siteRouter);
 
     app.get('*', (req, res) => {
         res.render('404');
     });
-    
 }
 
 module.exports = routes;
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 const newController = require('../app/controllers/NewsController');
 
-router.use('/:slug', newController.detail);
-router.use('/', newController.index);
+router.get('/:slug', newController.detail);
+router.get('/', newController.index);
 
 module.exports = router;
